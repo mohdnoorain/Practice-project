f6a23bb873fccb067f297e2d6f7d3d29a6f6a418
Md. Noorain <md.noorain9555@gmail.com>
2022-05-23 13:02:10 +0530

    window size issue fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DefaultModule } from './features/default/default.module';
 import { MainBoardModule } from './features/main-board/main-board.module';
 
 
 const routes: Routes = [
-  { path: "", redirectTo: "default", pathMatch: "full" },
-  // {
-  //   path: "mainboard",
-  //   loadChildren: () => import("./features/main-board/main-board.module").then( m => m.MainBoardModule)
-  // },
-  // {
-    //   path: "default",
-    //   loadChildren: () => import("./features/default/default.module").then((m) => m.DefaultModule)
-    // },
-  
+  { path: "", redirectTo: "mainboard", pathMatch: "full" },
   {
     path: "mainboard",
     loadChildren: () => MainBoardModule
-  },
-  {
-    path: "default",
-    loadChildren: () => DefaultModule
   },
 ];
 
